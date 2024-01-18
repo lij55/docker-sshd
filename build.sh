52da1749f73acbdc554be9934dddf284e1e0cb52
Jasper Li <liyang.jasper@gmail.com>
2024-01-18 17:19:46 +0800

    add kylin

--- build.sh
+++ build.sh
@@ -2,7 +2,9 @@
 
 docker build -f euler2203/Dockerfile -t euler2203 .
 
-docker build -f ubuntu2204/Dockerfile -t ubuntu2204 .
+docker build -f ubuntu2204/Dockerfile -t  lyasper/ubuntu2204 .
 
-docker build -f kylin_sp2/Dockerfile  -t kylinsp2 .
+docker build -f kylin_sp2/Dockerfile  -t lyasper/kylinsp2 .
 
+docker build -f rocky8/Dockerfile -t lyasper/rocky8 .
+docker build -f rocky9/Dockerfile -t lyasper/rocky9 .
