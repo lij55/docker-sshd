e222117ef0e8c42eda24850240cb7cd146391de8
lij55 <liyang.jasper@gmail.com>
2024-10-08 09:08:45 +0800

    add debian and ubuntu 2404

--- build.sh
+++ build.sh
@@ -3,6 +3,10 @@
 docker build -f euler2203/Dockerfile -t euler2203 .
 
 docker build -f ubuntu2204/Dockerfile -t  lyasper/ubuntu2204 .
+docker build -f ubuntu2204/Dockerfile -t  lyasper/ubuntu2404 .
+
+docker build -f debian11/Dockerfile -t  lyasper/debian11 .
+docker build -f debian12/Dockerfile -t  lyasper/debian12 .
 
 docker build -f kylin_sp2/Dockerfile  -t lyasper/kylinsp2 .
 
